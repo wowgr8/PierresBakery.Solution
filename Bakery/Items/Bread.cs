c1e889bc86b97d5a4451e9e63ad8df9a7cae08a6
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-11 23:06:29 -0800

    add GetQuantity method

--- Bakery/Items/Bread.cs
+++ Bakery/Items/Bread.cs
@@ -2,14 +2,19 @@ using System;
 
 namespace Bakery.Items
 {
-  public class Bread
+  public class Bread //This is a class
   {
-    public int Price {get; set;}
+    public int Price {get; set;} //This is a field
+    public int Quantity {get; set;}
 
-    public Bread(int breadPrice)
+    public Bread(int breadPrice) // This is a constructor
     {
       Price = breadPrice;
     }
 
+    public int GetQuantity() // This is a method
+    {
+      return Quantity = Price / 5 ;
+    }
   }
 }
