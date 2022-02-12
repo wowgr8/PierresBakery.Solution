d5c9b50ce94f14c04d37ba491b7ea83352811ed0
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-12 11:48:50 -0800

    add GetPrice method and Price field to class

--- Bakery/Items/Pastry.cs
+++ Bakery/Items/Pastry.cs
@@ -5,10 +5,17 @@ namespace Bakery.Items
   public class Pastry
   {
     public int Quantity {get; set;}
+    public int Price {get; set;}
 
     public Pastry(int pastryQuantity)
     {
       Quantity = pastryQuantity;
+      Price = Quantity;
+    }
+
+    public int GetPrice()
+    {
+      return Price * 2;
     }
   }
 }
