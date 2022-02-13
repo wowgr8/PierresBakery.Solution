bc63b7445ad2fe4f712e9502235063458f69aaeb
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-12 16:02:43 -0800

    add using statements, namespace, public class, and initial Console.WriteLines greeting user

---
+++ Bakery/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using Bakery.Items;
+
+namespace Bakery
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Welcome to Pierre's Bakery!");
+      Console.WriteLine("Welcome to Pierre's Bakery!")
+      
+    }
+  }
+}
