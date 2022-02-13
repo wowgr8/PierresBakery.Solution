f15cd849109bf775e76059b78f378632af937658
Cesar Lopez <Lopez.cesar.aug@gmail.com>
2022-02-12 17:00:54 -0800

    removed comments

--- Bakery.Tests/ItemsTests/BreadTests.cs
+++ Bakery.Tests/ItemsTests/BreadTests.cs
@@ -10,57 +10,40 @@ namespace Bakery.Tests
     [TestMethod]
     public void BreadConstructor_CreateInstanceOfBread_Bread()
     {
-      //Arrange
       int sampleQuantity = 1;
-      //Act      
       Bread loafBread = new Bread(sampleQuantity);
-      //Assert
       Assert.AreEqual(typeof(Bread), loafBread.GetType());
     }
 
     [TestMethod]
     public void BreadConstructor_SetQuantityProperty_1()
     {
-      //Arrange
       int sampleQuantity = 1;
-
-      //Act
       Bread loafBread = new Bread(sampleQuantity);
-
-      //Assert
       Assert.AreEqual(loafBread.Quantity, 1);
     }
 
     [TestMethod]
     public void GetPrice_GetBreadPrice_5()
     {
-      //Arrange
       int sampleQuantity = 1;
-      //Act
       Bread loafBread = new Bread(sampleQuantity);
-      //Assert
       Assert.AreEqual(loafBread.GetPrice(), 5);
     }
 
     [TestMethod]
     public void GetPrice_DiscountEveryThirdQuantity_10()
     {
-      //Arrange
       int sampleQuantity = 3;
-      //Act
       Bread loafBread = new Bread(sampleQuantity);
-      //Assert
       Assert.AreEqual(loafBread.GetPrice(), 10);
     }
 
     [TestMethod]
     public void GetPrice_DiscountQuantityContainingAnyVariablesOfThree_15()
     {
-      //Arrange
       int sampleQuantity = 4;
-      //Act
       Bread loafBread = new Bread(sampleQuantity);
-      //Assert
       Assert.AreEqual(loafBread.GetPrice(), 15);
     }
   }
